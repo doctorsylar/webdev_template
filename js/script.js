5a3ad4ee1d8055862418e8234066a1fc663d4602
Andrey <soul.evans.15@gmail.com>
2018-07-18 22:58:55 +0300

    added fixed home right-button corner of the window

--- js/script.js
+++ js/script.js
@@ -14,12 +14,28 @@ menuToggler.onclick = function () {
 window.sr = ScrollReveal();
 sr.reveal('.animated');
 
+
 // jQuery
 $(function () {
     $('.header-menu .menu-item a').click(function (event) {
         event.preventDefault();
         let id = $(this).attr('href');
         let top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 500);
+        $('body,html').animate({scrollTop: top}, 800);
+    });
+    $('#home').click(function (event) {
+        event.preventDefault();
+        $('body,html').animate({scrollTop: 0}, 300);
+    });
+    $(window).scroll(function () {
+        let homeButton = document.getElementById('home');
+        let scrollPos = window.pageYOffset;
+        if (scrollPos > 200 &&
+            window.getComputedStyle(homeButton).display === 'none') {
+            homeButton.style.display = 'block';
+        }
+        else if (scrollPos < 200) {
+            homeButton.style.display = 'none';
+        }
     });
 });
